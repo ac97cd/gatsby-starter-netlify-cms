012307098369d5438026f1621e5a6d46eabdd82a
ac97cd <41847906+ac97cd@users.noreply.github.com>
2022-03-05 20:07:09 +0800

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ const NotFoundPage = () => (
   <Layout>
     <div>
       <h1>NOT FOUND</h1>
-      <p>Sorry. The page is unavailable :(</p>
+      <p>"Sorry. The page is unavailable :("</p>
       <p><img src="../img/unavailable.png" alt="Page unavailable"></p>
     </div>
   </Layout>
