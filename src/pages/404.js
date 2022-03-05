7c43d9482c06e244bda550bfa0128421caf00b0b
ac97cd <41847906+ac97cd@users.noreply.github.com>
2022-03-05 19:39:31 +0800

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,8 @@ const NotFoundPage = () => (
   <Layout>
     <div>
       <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>Sorry. The page is unavailable :(</p>
+      <img>../img/'Unavailable Page.png'</img>
     </div>
   </Layout>
 )
