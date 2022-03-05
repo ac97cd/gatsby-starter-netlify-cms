a77f4b86e2fce0a908e303f628fb4fdc6d3268ba
ac97cd <41847906+ac97cd@users.noreply.github.com>
2022-03-05 21:40:58 +0800

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,13 +4,17 @@ import unavailable from '../img/unavailable.png'
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
+    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
       <h1>URL Address Not Found</h1>
       <p>Sorry. The page is unavailable :(</p>
     </div>
 
     <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-      <img src={unavailable} alt="Page unavailable"/>
+      <p><img src={unavailable}
+              alt="Page unavailable"
+              height={500}
+              />
+      </p>
     </div>
   </Layout>
 )
