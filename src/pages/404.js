3619605896c2e5e810eaa718f7b72ed6f32e3269
ac97cd <41847906+ac97cd@users.noreply.github.com>
2022-03-05 21:09:57 +0800

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import Image from 'react-image-resizer';
 import unavailable from '../img/unavailable.png'
 
 const NotFoundPage = () => (
@@ -8,8 +7,7 @@ const NotFoundPage = () => (
     <div>
       <h1>NOT FOUND</h1>
       <p>"Sorry. The page is unavailable :("</p>
-      <p><Image
-            img src={unavailable} alt="Page unavailable"
+      <p><img src={unavailable} alt="Page unavailable"
             height={500}
             width={300}
             style={{ alignSelf: 'center'}}
